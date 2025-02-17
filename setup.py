3372637e3718661e6c7d6b7457c5a93b04c0f695
SUBHAKSHAN CHAKRABORTY <167389887+Subhakshan-Chakraborty@users.noreply.github.com>
2025-02-17 11:18:22 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MedicalChatbotGenAI',
+    version= '0.1',
+    author= 'Subhakshan Chakraborty',
+    author_email= 'subhakshanchakraborty8@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
